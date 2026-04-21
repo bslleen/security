31fa82b8500043df0fc5d8f45fd09e8293b60515
bouchra <soundouszem@gmail.com>
2026-04-21 21:49:53 +0100

    update crypto utils

--- common/crypto_utils.py
+++ common/crypto_utils.py
@@ -1,9 +1,6 @@
 import random
 import hashlib
 
-# -----------------------------
-# 1. Generate Prime Numbers
-# -----------------------------
 def is_prime(n):
     if n < 2:
         return False
@@ -14,45 +11,39 @@ def is_prime(n):
 
 def generate_prime():
     while True:
-        num = random.randint(100, 300)
+        num = random.randint(10**12, 10**13)  
         if is_prime(num):
             return num
 
-# -----------------------------
-# 2. GCD + Inverse
-# -----------------------------
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
 
-def mod_inverse(e, phi):
-    for d in range(1, phi):
-        if (d * e) % phi == 1:
-            return d
-    return None
+def mod_inverse(e, phi):                     
+    original_phi = phi
+    x0, x1 = 0, 1
+    if phi == 1:
+        return 0
+    while e > 1:
+        q = e // phi
+        e, phi = phi, e % phi
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += original_phi
+    return x1
 
-# -----------------------------
-# 3. RSA Key Generation
-# -----------------------------
 def generate_keys():
     p = generate_prime()
     q = generate_prime()
-
     n = p * q
     phi = (p - 1) * (q - 1)
-
     e = 3
     while gcd(e, phi) != 1:
         e += 2
-
     d = mod_inverse(e, phi)
-
     return (e, n), (d, n)
 
-# -----------------------------
-# 4. Encrypt / Decrypt
-# -----------------------------
 def encrypt_vote(message, public_key):
     e, n = public_key
     m = int.from_bytes(message.encode(), 'big')
@@ -62,29 +53,17 @@ def encrypt_vote(message, public_key):
 def decrypt_vote(cipher, private_key):
     d, n = private_key
     m = pow(cipher, d, n)
-    message = m.to_bytes((m.bit_length() + 7) // 8, 'big').decode()
-    return message
+    return m.to_bytes((m.bit_length() + 7) // 8, 'big').decode()
 
-# -----------------------------
-# 5. Hash (SHA-256)
-# -----------------------------
 def hash_data(data):
     return int(hashlib.sha256(str(data).encode()).hexdigest(), 16)
 
-# -----------------------------
-# 6. Signature
-# -----------------------------
 def sign_data(data, private_key):
     d, n = private_key
-    hashed = hash_data(data)
-    signature = pow(hashed, d, n)
-    return signature
+    hashed = hash_data(data) % n             
+    return pow(hashed, d, n)
 
-# -----------------------------
-# 7. Verify Signature
-# -----------------------------
 def verify_signature(data, signature, public_key):
     e, n = public_key
-    hashed = hash_data(data)
-    decrypted_hash = pow(signature, e, n)
-    return hashed == decrypted_hash
+    hashed = hash_data(data) % n             
+    return hashed == pow(signature, e, n)
