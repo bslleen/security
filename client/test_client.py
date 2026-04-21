5093e326b3f26a619d82f9c26ff54e367d30db6e
Rim <198073685+rimxii@users.noreply.github.com>
2026-04-21 20:55:44 +0200

    Updated

--- client/test_client.py
+++ client/test_client.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
+
 import socket
 import json
 import os
 import sys
-
-# Ensure project root is in path for imports
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from common.crypto_utils import encrypt_vote, sign_data
 
 HOST = '127.0.0.1'
 PORT = 12345
 
-from common.crypto_utils import encrypt_vote, sign_data
-
 SERVER_PUBLIC_KEY = (17, 3245)
 VOTER_PRIVATE_KEY = (2353, 3245)
+
 
 def send_request(data):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
             s.send(json.dumps(data).encode('utf-8'))
-            response = s.recv(4096).decode('utf-8')
-            return response
+            return s.recv(4096).decode('utf-8')
     except Exception as e:
-        print(f"[ERROR] Connection failed: {e}")
+        print(f"[ERROR] {e}")
         return None
 
-def create_vote_packet(student_id, candidate):
+
+def create_vote(student_id, candidate):
     ciphertext = encrypt_vote(candidate, SERVER_PUBLIC_KEY)
     signature = sign_data(ciphertext, VOTER_PRIVATE_KEY)
 
@@ -36,53 +36,19 @@ def create_vote_packet(student_id, candidate):
         "signature": signature
     }
 
-def option1_manual_vote():
-    student_id = input("Enter student_id: ").strip()
-    candidate = input("Enter candidate (Candidat_A/B/C): ").strip()
-
-    packet = create_vote_packet(student_id, candidate)
-    response = send_request(packet)
-
-    if response:
-        print(f"[SERVER] {response}")
-
-def option2_simulate():
-    print("Simulating votes...\n")
-
-    tests = [
-        ("Ali123", "Candidat_A"),
-        ("Ali456", "Candidat_B"),
-        ("Ali789", "Candidat_A"),
-        ("Ali123", "Candidat_C"),
-        ("FakeID", "Candidat_A")
-    ]
-
-    for i, (student_id, candidate) in enumerate(tests, 1):
-        print(f"Test {i}: {student_id} -> {candidate}")
-
-        packet = create_vote_packet(student_id, candidate)
-        response = send_request(packet)
-
-        print(f"Result: {response}\n")
 
 def main():
-    print("Secure E-Voting Client\n")
+    print("Client ready\n")
 
     while True:
-        print("1. Send vote")
-        print("2. Simulate election")
-        print("0. Exit")
+        student_id = input("ID: ").strip()
+        vote = input("Vote (Candidat_A/B/C): ").strip()
 
-        choice = input("Choice: ").strip()
+        packet = create_vote(student_id, vote)
+        response = send_request(packet)
 
-        if choice == '1':
-            option1_manual_vote()
-        elif choice == '2':
-            option2_simulate()
-        elif choice == '0':
-            break
-        else:
-            print("Invalid choice\n")
+        print(response, "\n")
+
 
 if __name__ == "__main__":
     main()
